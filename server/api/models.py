d61cb4ed822216156703d8b0db6ccb36111db888
hokoro <hokoro123@gmail.com>
2023-12-18 14:35:00 +0900

    Custom User 모델 생성

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, user_id, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('date_joined', timezone.now())
+
+        if not user_id:
+            raise ValueError("유저 아이디를 반드시 입력해주세요")
+
+        user = self.model(user_id=user_id, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, user_id, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('date_joined', timezone.now())
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("관리자는 무조건 관계자이다.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('관리자는 무조건 superuser 이다.')
+
+        return self.create_user(user_id, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # custom field
+    # id , pw , username ,
+    id = models.CharField(max_length=30, unique=True, null=False, blank=False, primary_key=True)
+    password = models.CharField(max_length=20, null=False, blank=False)
+    name = models.CharField(max_length=30, null=False, blank=False)
+    phone = models.CharField(max_length=15, null=False, blank=False)
+    token = models.CharField(max_length=70, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    # Username = 유저의 아이디 = 다시 재정의  , Required_field
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'id'
+    REQUIRED_FIELDS = ['password', 'name', 'phone']
+
+    def __str__(self):
+        return self.id
+
+
+class post(models.Model):
+    pass
--- server/hand_signs/settings.py
+++ server/hand_signs/settings.py
@@ -41,6 +41,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+AUTH_USER_MODEL = 'api.User'
 
 # Application definition
 
@@ -119,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -137,3 +138,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
