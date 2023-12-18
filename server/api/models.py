0f6231c98cd0071c1e9a4b3c14f1f673593255dc
SuYoungSong <prodraft@naver.com>
2023-12-18 15:17:28 +0900

    API models.py에 ERD 기반 테이블 제작

--- server/api/models.py
+++ server/api/models.py
@@ -34,6 +34,7 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(user_id, password, **extra_fields)
 
 
+# User 객체
 class User(AbstractBaseUser, PermissionsMixin):
     # custom field
     # id , pw , username ,
@@ -58,5 +59,42 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.id
 
 
-class post(models.Model):
-    pass
+# 질문 게시판 객체
+class question_board(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(null=False, blank=False)
+    state = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+# 질문 게시판 객체 - 이미지
+class question_board_images(models.Model):
+    board_id = models.ForeignKey(question_board, on_delete=models.CASCADE)
+    image_url = models.ImageField(upload_to='question_board_images/')
+
+# 질문 게시판 객체 - 답변
+class question_board_comments(models.Model):
+    board_id = models.ForeignKey(question_board, on_delete=models.CASCADE)
+    comment = models.TextField(null=False, blank=False)
+
+
+# 완료한 챕터 기록
+class education_report(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    chapter = models.IntegerField(null=False, blank=False)
+
+# 학습지(문제지)를 저장하는 객체
+class paper(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    situation = models.CharField(max_length=8 , null=False, blank=False)
+    sign_video_url = models.URLField(null=False, blank=False)
+    sign_answer = models.CharField(max_length=8, null=False , blank=False)
+
+# 틀린 문제를 저장하는 객체
+class wrong_answer_notes(models.Model):
+    paper_id = models.IntegerField(null=False, blank=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# 맞춘 문제를 저장하는 객체
+class answer_notes(models.Model):
+    paper_id = models.IntegerField(null=False, blank=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
