96ff8bfb0bc5410059eb7ece1a37cd6c5e8ad3e2
subbie <2lsbapple@gmail.com>
2024-01-03 03:22:57 +0900

    edit component

---
+++ client/my-app/src/app/components/inputwithbtn.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import '@/app/auth/auth.css'
+
+interface InputPropsWB{
+    id: string;
+    onChange: any;
+    value: string;
+    label: string;
+    type?: string;
+    onclick?: any;
+    btntext: string;
+    speclassName?:any;
+    spetextclassName?:any;
+}
+
+const InputWB: React.FC<InputPropsWB> = ({
+    id,
+    onChange,
+    value,
+    label,
+    type,
+    onclick,
+    speclassName,
+    btntext,
+    spetextclassName,
+}) => {
+    return (
+        <>
+            <div id="info_id">
+                <div className="wb-input-div">
+                    <input
+                        onChange={onChange}
+                        type={type}
+                        value={value}
+                        id={id}
+                        className="wb-input-style"
+                        placeholder=""
+                    />
+                    <label
+                        className="label-style"
+                        htmlFor={id}>
+                        {label}
+                    </label>
+                </div>
+                <button className={speclassName? speclassName:"check_username"} onClick={onclick}> <div className={spetextclassName? spetextclassName:'wb-btn-text'}>{btntext}</div></button>
+            </div>
+        </>
+    )
+}
+export default InputWB;
