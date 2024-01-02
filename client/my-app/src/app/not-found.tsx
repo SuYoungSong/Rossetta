2d42f7f587f26d74486046bf86382ff3dd90a9c8
subbie <2lsbapple@gmail.com>
2024-01-02 11:32:57 +0900

    404 page css change

--- client/my-app/src/app/not-found.tsx
+++ client/my-app/src/app/not-found.tsx
@@ -3,7 +3,7 @@ import {NextPage} from "next";
 
 const NotFound: NextPage = () => {
     return (
-        <div>
+        <div style={{marginTop:500}}>
             <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.</div>
             <Link href="@/app/sign_edu">홈</Link>
         </div>
