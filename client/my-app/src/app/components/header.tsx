6a0e237fdc1f17bdc707255ac9b0ef3dfcdf5373
SuYoungSong <prodraft@naver.com>
2024-01-04 21:38:06 +0900

    [ header.tsx ] - 현재 로그인 시간이 유효한지 로직을 추가하였습니다. - 1분마다 서버에 POST 요청을 보내고 정상적인 응답이 오면 로그인을 유지하고 특정 시간이 경과하여 로그인 유효시간을 경과한 경우 로그아웃 로직이 실행되게 하였습니다.

--- client/my-app/src/app/components/header.tsx
+++ client/my-app/src/app/components/header.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Inter } from 'next/font/google';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import '@/app/styles/globals.css'
@@ -34,7 +35,39 @@ const Header = ({username, status, token}: HeaderProps) => {
       .catch((err) => {
         console.log("err >> ", err);
       });
-};
+    };
+
+    // 자동 로그아웃 API 를 체크하는 함수
+    const autoLogoutTimeCheck = () => {
+        let accessToken = localStorage.getItem('accessToken');
+        axios.post("http://localhost:8000/api/autologout/", null, {headers: {'Authorization': `Token ${accessToken}`}})
+            .then((res) => {
+                // 정상적으로 200 응답이 온 경우 로그인 시간이 유효하기 때문에
+                // 아무 작업을 하지 않습니다.
+                // 아무것도 없다고 놀라지 마세요 이게 맞아요.
+            })
+            .catch((err) => {
+                // 404 에러코드가 응답으로 오는 경우
+                // 로그인 유효시간이 만료로 logout 처리합니다.
+                logout();
+            });
+    }
+
+    useEffect(() => {
+        // 1분마다 실행되는 setInterval 설정
+        const autoLogoutIntervalId = setInterval(() => {
+            // 로그인 시간이 유효한지 체크하는 POST request
+            // 로그인 한 경우에만 체크하게 accessToken 값이 있는지 없는지 체크 후 작동한다.
+            if (localStorage.getItem('accessToken')) {
+                autoLogoutTimeCheck();
+            }
+        }, 1 * 60 * 1000); // 1분(60,000 밀리초)
+
+        // 컴포넌트가 언마운트되면 clearInterval을 사용하여 interval을 중지합니다.
+        return () => clearInterval(autoLogoutIntervalId);
+    }, []);
+
+
 
     return (
         <>
@@ -55,7 +88,7 @@ const Header = ({username, status, token}: HeaderProps) => {
                       {/*</div>*/}
                       <div className='user_name'>
                           <Dropdown label={`${username}님`} inline className="dropdown-container">
-                              <Dropdown.Item icon={HiCog}><Link href='/mypage'>내 정보</Link></Dropdown.Item>
+                              <Link href='/mypage'><Dropdown.Item icon={HiCog}>내 정보</Dropdown.Item></Link>
                               <Dropdown.Divider className="divider-drop"/>
                               <Dropdown.Item icon={HiLogout} onClick={logout}>로그아웃</Dropdown.Item>
                             </Dropdown>
