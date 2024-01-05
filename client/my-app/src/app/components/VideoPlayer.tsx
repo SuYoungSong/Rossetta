e92abb0b1de57ac36f4b8a8413320a13bd7b41cd
subbie <2lsbapple@gmail.com>
2024-01-05 17:13:54 +0900

    비디오형식변경

--- client/my-app/src/app/components/VideoPlayer.tsx
+++ client/my-app/src/app/components/VideoPlayer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import "@/app/styles/situation_num.css"
 
 interface VideoProps {
-    src_url: string[];
-    count: number;
+    src_url: string | string[];
+    count?: number;
 }
 
 const VideoPlayer: React.FC<VideoProps> = ({src_url, count}) => {
@@ -11,7 +11,7 @@ const VideoPlayer: React.FC<VideoProps> = ({src_url, count}) => {
   return (
     <>
         <div className="outer">
-            <video controls className="video" src={src_url[count]}></video>
+            <video controls className="video" src={src_url[count ?? 0]}></video>
         </div>
     </>
   );
