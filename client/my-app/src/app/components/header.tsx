eef3b302ee3a8395c08440cf42e2c5ae70d87b85
subbie <2lsbapple@gmail.com>
2024-01-05 17:13:46 +0900

    add chatbot

--- client/my-app/src/app/components/header.tsx
+++ client/my-app/src/app/components/header.tsx
@@ -109,7 +109,7 @@ const Header = ({username, status, token}: HeaderProps) => {
             <div className="nav_basic">
                 <Link  href="/"><Image className="logo" src={RoLogo} alt='logo'/></Link>
                 <Link className='nav_btn'  href='/sign-edu'>수어교육</Link>
-                <Link className='nav_btn'  href='/'>수어실습</Link>
+                <Link className='nav_btn'  href='/chatbot'>수어실습</Link>
                 <Link className='nav_btn'  href='/wrongnote'>오답노트</Link>
                 <Link className='nav_btn'  href='/board'>1:1 문의</Link>
             </div>
