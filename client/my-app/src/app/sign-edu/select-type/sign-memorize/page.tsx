4c1e20276d509e1c50f1964168d57d0e9174191b
subbie <2lsbapple@gmail.com>
2024-01-05 17:54:48 +0900

    add Check part

--- client/my-app/src/app/sign-edu/select-type/sign-memorize/page.tsx
+++ client/my-app/src/app/sign-edu/select-type/sign-memorize/page.tsx
@@ -1,12 +1,17 @@
 "use client"
 
 import WebCam from "@/app/components/webCam";
+import getAnswer from "@/app/components/getAnswer";
 import React from "react";
 import { useSearchParams } from 'next/navigation';
 import axios from "axios";
 import { useState } from "react";
+import Image from "next/image";
+import idkImage from "../../../../../public/idknow.png";
+import CorrectImage from "../../../../../public/correct_image.png";
+import IncorrectImage from "../../../../../public/incorrect_image.png";
 export default function SignMemory() {
-    const isAnswerCorrect = true
+    const isAnswerCorrect = false
     const searchParams = useSearchParams()
 
     const type = searchParams.get('type')
@@ -41,21 +46,32 @@ export default function SignMemory() {
                 });
         };
         getQuestion();
-        const getAnswer = () => {
+        const getAnswers = () => {
             if (isAnswerCorrect === null) {
-                return {text: '답변 대기중', color: 'gray'};
+              return { text: '모르겠어요', color: '#FFE6B5', image: idkImage };
             } else if (isAnswerCorrect) {
-                return {text: '정답입니다!', color: 'red'};
+              return { text: '정답', color: '#D0E8FF', image: CorrectImage };
             } else {
-                return {text: '틀렸습니다.', color: 'blue'};
+              return { text: '오답', color: '#FFC7C7', image: IncorrectImage };
             }
-        };
+          };
 
-        const getAnswers = getAnswer();
+        const getAnswer = getAnswers();
 
         return (
             <>
-                <WebCam answer={answer}/>
+                <div className="whole_camera">
+                    <WebCam answer={answer}/>
+                    <div className="answer_btn">
+                        <div className="question">
+                            <div className="quest-text">단어</div>
+                        </div>
+                        <div className="check" style={{backgroundColor: getAnswer.color}}>
+                            <div className="answer_text">{getAnswer.text}</div>
+                            <Image className="answer_img" src={getAnswer.image} alt="answer_img"/>
+                        </div>
+                    </div>
+                </div>
             </>
         );
-    }
+}
